7441c91895e6121c1c08e1e23316f47859307647
chou-shinka <chou-shinka@DESKTOP-TVT8LDP>
2018-08-06 20:20:20 +0100

    Base version

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace map_caster
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void open_map_btn_Click(object sender, RoutedEventArgs e)
+        {
+            MapWindow mapWindow = new MapWindow();
+
+            if (!mapWindow.IsInitialized)
+                mapWindow.InitializeComponent();
+
+            mapWindow.Show();
+        }
+    }
+}
---
+++ MapWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace map_caster
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class MapWindow : Window
+    {
+        public MapWindow()
+        {
+            InitializeComponent();
+
+            // Create a canvas sized to fill the window
+            Canvas myCanvas = new Canvas();
+            myCanvas.Background = Brushes.LightSteelBlue;
+
+            this.Content = myCanvas;
+            this.Title = "Canvas Sample";
+            this.Show();
+        }
+    }
+}
